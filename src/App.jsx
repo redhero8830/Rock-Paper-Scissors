fa558e0880b322e66e1098c1ad8902351049f941
Mark <redhero8830@gmail.com>
2024-03-21 00:35:07 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,73 @@ import Box from "./components/Box";
 const choice = {
   rock: {
     name: "Rock",
-    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Dwayne_%22The_Rock%22_Johnson_Visits_the_Pentagon_%2841%29_%28cropped%29.jpg/440px-Dwayne_%22The_Rock%22_Johnson_Visits_the_Pentagon_%2841%29_%28cropped%29.jpg",
+    img: "https://partyzzang.co.kr/web/product/small/202305/db16fba4ff16fb26ba1de406a27c2d10.jpg",
   },
   paper: {
-    name: "Rock",
-    img: "https://pandapaperroll.com/wp-content/uploads/2022/03/A4-letterhead-printing-header-paper.jpg",
+    name: "Paper",
+    img: "https://lh3.googleusercontent.com/proxy/YOEzI26N4HZnGXBzMlqYu2Z9S-M5Uyjn0kmGA8pzPpLfdCA0rFrtg4kkR-7CG0wQ2hKN2-VwynspzEok4UKEg8t1uJoM5alzo2Y_QlAsNRh-H38FQlRWKA",
   },
   scissor: {
-    name: "Rock",
-    img: "https://i.ebayimg.com/images/g/Sf8AAOSwDINipyBs/s-l1600.jpg",
+    name: "Scissors",
+    img: "https://thumbnail.10x10.co.kr/webimage/image/basic600/196/B001969629-4.jpg?cmd=thumb&w=400&h=400&fit=true&ws=false",
   },
 };
 
 function App() {
   const [userSelect, setUserSelect] = useState(null);
+  const [comSelect, setComSelect] = useState(null);
+  const [result, setResult] = useState("");
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComSelect(computerChoice);
+    judgement(choice[userChoice], computerChoice);
+  };
+
+  const randomChoice = () => {
+    //객체를 랜덤으로 선택하는 알고리즘
+    let itemArray = Object.keys(choice); //Object.keys -> 객체의 키 값만 뽑아서 배열로 만들어주는 함수
+    // console.log(itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+    //처음에는 당연히 모름..
+    //어려운 것부터 하면서 이해와 함께 외워짐
+  };
+
+  const judgement = (user, com) => {
+    console.log(user, com);
+    if (user.name === com.name) {
+      setResult("Tie");
+    } else if (user.name === "Rock") {
+      com.name === "Scissors" ? setResult("Win") : setResult("Lose");
+    } else if (user.name === "Paper") {
+      com.name === "Rock" ? setResult("Win") : setResult("Lose");
+    } else if (user.name === "Scissors") {
+      com.name === "Paper" ? setResult("Win") : setResult("Lose");
+    }
   };
 
   return (
-    <>
+    <div className="container">
+    <h1>Rock Paper Scissors</h1>
       <div className="main">
-        <Box title="User" item={userSelect} />
-        <Box title="Computer" />
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={comSelect} result={result} />
       </div>
-      <div className="main">
-        <button onClick={() => play("scissor")}>가위</button>
-        <button onClick={() => play("rock")}>바위</button>
-        <button onClick={() => play("paper")}>보</button>
+      <div className="buttons">
+        <button onClick={() => play("scissor")}>
+          <img src="https://thumbnail.10x10.co.kr/webimage/image/basic600/196/B001969629-4.jpg?cmd=thumb&w=400&h=400&fit=true&ws=false" alt="scissors"/>
+        </button>
+        <button onClick={() => play("rock")}>
+        <img src="https://partyzzang.co.kr/web/product/small/202305/db16fba4ff16fb26ba1de406a27c2d10.jpg" alt="paper"/>
+        </button>
+        <button onClick={() => play("paper")}>
+        <img src="https://lh3.googleusercontent.com/proxy/YOEzI26N4HZnGXBzMlqYu2Z9S-M5Uyjn0kmGA8pzPpLfdCA0rFrtg4kkR-7CG0wQ2hKN2-VwynspzEok4UKEg8t1uJoM5alzo2Y_QlAsNRh-H38FQlRWKA" alt="rock"/>
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-export default function Box(props) {
-  console.log(props);
+export default function Box({ title, item, result }) {
+  let newResult;
+  if (title === "Computer" && result !== "Tie" && result !== "") {
+    newResult = result === "Win" ? "Lose" : "Win";
+  } else {
+    newResult = result;
+  }
   return (
-    <div className="box">
-      <h1>{props.title}</h1>
+    <div className={`box ${newResult}`}>
+      <h1>{title}</h1>
       {/* useSelect 값은 유저가 선택하고 들어옴, 처음 값음 null */}
       {/* 그런데 처음부터 box는 렌더링을 하긴 해야하는데, null이면 못 그림 -> && 연산자로 가드 */}
       {/* 처음 값이 null -> props.item = null -> falsy ->  */}
-      <img className="box-img" src={props.item && props.item.img} alt="" />
-      <h1>Win</h1>
+      <img className="box-img" src={item && item.img} alt="" />
+      {/* <img className="box-img" src={item && item.img} alt="" /> */}
+      <h2>{newResult}</h2>
     </div>
   );
 }
